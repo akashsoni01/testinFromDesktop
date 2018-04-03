97818627de1b9c505ef2d983316b7dcbd9e76405
akash9888soni <akashsoni8285@gmail.com>
2018-04-03 07:04:44 +0530

    testing form branch commit

---
+++ code.js
@@ -0,0 +1,5 @@
+// making js code external js file
+// past the code of script tag
+//here alert is the function of a object windo  that is window
+window.alert("using alert with the help of object")
+	alert("Welcome to java script")
